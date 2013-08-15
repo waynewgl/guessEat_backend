02afbb410e86600cc0446abad06ffcd4d042c5b9
way <waynewgl@gmail.com>
2013-08-15 12:30:51 +0800

    first initial

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,11 @@
+class SiteController < ApplicationController
+  protect_from_forgery
+
+
+  def index
+
+
+
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GuessEatBackend::Application.config.secret_token = 'c4e6eb25dfc7237eba1d6e03fadcfe2f43e45e59ed81caa2d21afd73c3fe9c712ce611c79cb696447a24957483717bbe8161bda2a8b86d8fffa7e256571ce8b9'
